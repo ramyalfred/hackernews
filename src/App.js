cab5f7427b386f4ca17f53b43f8b4cc40917ef82
ramyalfred <ramy.alfred92@gmail.com>
2021-07-28 12:09:25 +0200

    Focusing the input field after mounting

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import loading from './assets/loading.svg'
 import './App.css';
 import { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import { render } from '@testing-library/react';
 
 
 const DEFAULT_QUERY = 'redux';
@@ -58,8 +60,8 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
       error: null,
+      isLoading: false,
     };
-
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
@@ -86,12 +88,13 @@ class App extends Component {
     //Combine current hits with previously stored hits
     const updatedHits = [...oldHits,...hits];
 
-    //Update the cache with the new results
+    //Update the cache with the new results and stop the Loading indicator
     this.setState({
       results: {
         ...results,
         [searchKey]: {hits: updatedHits,page}
-      }
+      },
+      isLoading: false,
     });
   };
 
@@ -126,7 +129,7 @@ class App extends Component {
   };
 
   render() {
-    const {results,searchTerm,searchKey,error} = this.state;
+    const {results,searchTerm,searchKey,error,isLoading} = this.state;
     const page = (results && results[searchKey] && results[searchKey].page) || 0;
     const list = (results && results[searchKey] && results[searchKey].hits) || [];
     
@@ -149,14 +152,19 @@ class App extends Component {
               list={list}
               onDismiss={this.onDismiss}
             />}
-          <Button onClick = {() => this.fetchSearch(searchKey,page+1)}>
-            More
-          </Button>
+          {isLoading
+            ? <Loading/>
+            : <Button 
+              onClick = {() => this.fetchSearch(searchKey,page+1)}>
+              More
+            </Button>}
         </div>
     );
   }
 
   fetchSearch(searchTerm,page = 0){
+    //Start loading indicator as the page loads
+    this.setState({isLoading: true});
 
     //Fetch the URL using axios library
     axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
@@ -177,19 +185,44 @@ class App extends Component {
   }
 }
 
-const Search = ({value,onChange,onSubmit,children}) => 
-    <form onSubmit={onSubmit}>
+//Refractor Search into an ES6 class component
+class Search extends Component{
+
+  constructor(props){
+    super(props);
+  }
+
+    render(){
+      const {value,onChange,onSubmit,children} = this.props;
+      return(
+        <form onSubmit={onSubmit}>
       {children}
       <input 
         type="text"
         onChange={onChange}
         value={value}
+        ref={(node) => {this.input = node}}
       />
       <button type='submit'>
           {children}
         </button>
     </form>
+      );
+    }
 
+    componentDidMount(){
+      if(this.input){
+        this.input.focus();
+      }
+    }
+}
+
+Search.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+}
 
 const Table = ({list,onDismiss}) =>
   <div className='table'>
@@ -213,6 +246,20 @@ const Table = ({list,onDismiss}) =>
   )}
   </div>
 
+// Defining types for Table props
+Table.propTypes = {
+  //Defining list in details
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })    
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+}
 
 const Button = ({onClick,className='',children}) =>
   <button
@@ -223,8 +270,18 @@ const Button = ({onClick,className='',children}) =>
     {children}
   </button>
 
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
 const Loading = () =>
-<img src={loading} alt='loading...'/>
+<img src={loading} alt='Loading...'/>
 
 export default App;
 
